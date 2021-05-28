f6d6fe12dde7c0673e2892d69a179e451d461f45
Mukul Saini <77503907+mukulsaini-dev@users.noreply.github.com>
2021-05-29 00:34:06 +0530

    add navbar

---
+++ my-app/src/page/home/components/navbar/nav.jsx
@@ -0,0 +1,55 @@
+import { BiSearch, BiArrowToBottom } from "react-icons/bi";
+import { IoIosArrowDropdownCircle } from "react-icons/io";
+import "./nav.css";
+
+export function NavBar()
+{
+    return (
+        <>
+         <nav className="nav">
+
+        <div className="nav__logo">
+          skateboard
+          <img src="" alt="" />
+        </div>
+
+        <div className="search">
+
+          <input
+            className="search__input"
+            type="text"
+            placeholder="Search"
+          >
+            
+
+          </input>
+           <button className="search__button">
+            <BiSearch/> 
+            </button>
+        </div>
+
+        <div class="nav__avatar avatar">
+            
+          <img
+          className="nav__avatar-image"
+            alt=""
+            src="https://pbs.twimg.com/profile_images/1372310949458112512/Isl5HmGT_400x400.jpg"
+          />
+
+          <span 
+          className="nav__avatar-name"
+         > Mukul </span>
+
+        </div>
+    
+        
+        <button className="nav__dropdown-menu">        
+          <IoIosArrowDropdownCircle 
+          />
+        </button>
+
+
+      </nav>
+        </>
+    )
+}
