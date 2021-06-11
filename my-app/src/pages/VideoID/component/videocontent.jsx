b363f35171e3089b68acc7235049903c836a7fb9
Mukul Saini <77503907+mukulsaini-dev@users.noreply.github.com>
2021-06-11 11:14:46 +0530

    connectbackend to watchLater page

--- my-app/src/pages/VideoID/component/videocontent.jsx
+++ my-app/src/pages/VideoID/component/videocontent.jsx
@@ -2,18 +2,55 @@ import { AiFillHeart } from "react-icons/ai";
 import { MdPlaylistAdd, MdWatchLater } from "react-icons/md";
 
 import { useData } from "../../../context/data/video";
+import { useAuth } from "../../../context/data/auth/auth";
+import axios from "axios";
+
 
 export function VideoHead({ name, views, likes, video }) {
   const {
     state: { watchLaterVideos },
     dispatch,
   } = useData();
+
+  const { userData, token } = useAuth();
+
   const { _id: id } = video;
 
   const checkVideoInWatchLater = watchLaterVideos.find(
     (video) => video._id === id
   );
 
+  async function addToWatchLaterVideoInServer(userID, token, videoID) {
+    const url =
+      `https://video-library-2.mukulsaini02.repl.co/v1/${userID}/watch-later`;
+    const headers = {
+      authorization: token,
+    };
+
+    const response = await axios.post(
+      url,
+      {
+        videoId: videoID,
+      },
+      { headers }
+    );
+    if (response.status === 200) {
+      return response.data.watchLaterVideo;
+    }
+  }
+
+  async function addToWatchLaterVideos(userID, token, videoID) {
+
+
+    const response = await addToWatchLaterVideoInServer(userID, token, videoID);
+
+    if (response) {
+      dispatch({
+        type: "ADD_TO_WATCH_LATER",
+        payload: response,
+      });
+    }
+  }
   return (
     <>
       <span className="video-content__name">{name}</span>
@@ -51,10 +88,7 @@ export function VideoHead({ name, views, likes, video }) {
                 <div className="video-content__icon">
                   <MdWatchLater
                     onClick={() =>
-                      dispatch({
-                        type: "ADD_TO_WATCH_LATER",
-                        payload: video,
-                      })
+                      addToWatchLaterVideos(userData._id, token, video._id)
                     }
                     className="icon"
                   />
