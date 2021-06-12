60b15472cc94d13efbd9a17edf3b7b756198d644
Mukul Saini <77503907+mukulsaini-dev@users.noreply.github.com>
2021-06-12 12:24:56 +0530

    refactor:player  ratio

--- my-app/src/pages/VideoID/component/player.jsx
+++ my-app/src/pages/VideoID/component/player.jsx
@@ -1,25 +1,23 @@
 import ReactPlayer from "react-player";
 
-export function VideoPlayer({videoId})
-{
-    return (
-        <>
-             <div className="video-content__player">
-              {/* 
+export function VideoPlayer({ videoId }) {
+  return (
+    <>
+      <div className="video-content__player">
+        {/* 
          here Aspect ratio is 16:9 
          
          */}
 
-              <ReactPlayer
-                className="react-player"
-                width="100%"
-                height="100%"
-                controls
-                playIcon
-                url={`https://www.youtube.com/watch?v=${videoId}`}
-              />
-            </div>
-        </>
-    )
+        <ReactPlayer
+          className="react-player"
+          width="100%"
+          height="100%"
+          controls
+          playIcon
+          url={`https://www.youtube.com/watch?v=${videoId}`}
+        />
+      </div>
+    </>
+  );
 }
-
