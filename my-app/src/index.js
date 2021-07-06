e9372f8db6639c23b542b8c03ba3e2e13e2407e0
Mukul Saini <77503907+mukulsaini-dev@users.noreply.github.com>
2021-07-06 11:12:13 -0700

    import watchLater and liked video providers

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,22 +3,27 @@ import ReactDOM from "react-dom";
 // import './index.css';
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { DataProvider } from "./context/data/video";
-import { AuthProvider } from "./context/data/auth/auth";
+import { DataProvider } from "./context/video/video";
+import { AuthProvider } from "./context/auth/auth";
+import WatchLaterProvider from "./context/watchLater/watchLater";
+import LikeVideosProvider from "./context/likeVideo/likeVideo";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <AuthProvider>
         <DataProvider>
-          <App />
+          <LikeVideosProvider>
+            <WatchLaterProvider>
+              <App />
+            </WatchLaterProvider>
+          </LikeVideosProvider>
         </DataProvider>
       </AuthProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
