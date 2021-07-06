14133acd4e5535b58dfb69c1362779b7b7fe9cc0
Mukul Saini <77503907+mukulsaini-dev@users.noreply.github.com>
2021-07-06 11:14:04 -0700

    added get watchLatersVideos api call

--- my-app/src/pages/watchLater/components/watchLaterVideos.jsx
+++ my-app/src/pages/watchLater/components/watchLaterVideos.jsx
@@ -1,13 +1,41 @@
-import { useData } from "../../../context/data/video";
+import { useData } from "../../../context/video/video";
 import { VideoCard } from "../../components/videocard/horizontal/videocard";
 import { WatchLaterRemoveOption } from "./dropDownRemoveButton";
-import { useAuth } from "../../../context/data/auth/auth";
-export function WatchLaterVideos() {
-  const { token, userData } = useAuth();
+import { useAuth } from "../../../context/auth/auth";
+import { useEffect } from "react";
+import { useWatchLater } from "../../../context/watchLater/watchLater";
+import axios from "axios";
 
-  const {
-    state: { watchLaterVideos },
-  } = useData();
+export function WatchLaterVideos() {
+
+  const { token, userData, userID , isUserLogin} = useAuth();
+  const {watchLaterVideos, watchLaterDispatch } = useWatchLater();
+  console.log({watchLaterVideos})
+
+  useEffect(()=>{
+    
+if(userID){
+
+
+    
+    (async function (){
+      console.log('watchLater useEffect running .... ')
+
+      const url = `https://videoLibraryBackend.imukulsaini.repl.co/v1/${userID}/watchlater`;
+      const headers ={
+        authorization:token
+      }
+      const response = await axios.get(url , {
+        headers
+      })
+      if(response.data.status === 201 ){
+        watchLaterDispatch({type:'INITIALIZE_WATCH_LATER_VIDEOS' , payload:response.data.watchLaterVideos})
+
+      }
+    })()
+
+}
+  },[])
 
   return watchLaterVideos.map((video) => {
     return (
