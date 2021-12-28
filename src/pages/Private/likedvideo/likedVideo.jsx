d58a2e1d46f0f735a73a6c49ecced7a2bc6d5185
Mukul Saini <77503907+imukulsaini@users.noreply.github.com>
2021-12-28 22:25:04 +0530

    add likeVideo page

---
+++ src/pages/Private/likedvideo/likedVideo.jsx
@@ -0,0 +1,70 @@
+import "./likedVideo.css";
+import { NavBar } from "../../components/Navbar/Nav";
+import { SideBar } from "../../components/Sidebar/Sidebar";
+import { useEffect } from "react";
+import { VideoCards } from "../../components/VideoCard/VideoCard";
+import { LikedVideoRemoveButton } from "./components/LikedVideoRemoveButton";
+import { useLike } from "../../../context/likeVideo/likeVideo";
+import { useAuth } from "../../../context/auth/auth";
+import { getUserLike } from "../../../api/api";
+import { useState } from "react/cjs/react.development";
+import { HeadingMain } from "../../components/HeadingMain/HeadingMain";
+import { LoadingSpinner } from "../../components/Spinner/LoadingSpinner";
+
+export function LikedVideo() {
+  const {
+    authState: { userID, token },
+  } = useAuth();
+  const [loading, setLoading] = useState("idle");
+  const [error, setError] = useState("");
+  const {
+    state: { likedVideos },
+    likeDispatch,
+  } = useLike();
+
+ 
+  useEffect(() => {
+    setLoading("pending");
+
+    if (userID) {
+      (async function () {
+        const response = await getUserLike(userID, token);
+        if (!response?.errMessage) {
+          setLoading("fulfilled");
+          likeDispatch({ type: "INITIALIZE_LIKE_VIDEOS", payload: response });
+        } else {
+          setLoading("rejected");
+          setError(response.errMessage);
+        }
+      })();
+    }
+  }, [userID, token]);
+  return (
+    <div className="like-video">
+      <NavBar />
+      <SideBar />
+      <section className="like__main">
+        <HeadingMain name={"Your Likes"} />
+        {loading === "pending" && <LoadingSpinner  
+        isDefaultCss={true}
+        size={30} />}
+        {loading === "fulfilled" &&
+          likedVideos &&
+          likedVideos?.map((video) => (
+            <div className="like-video__show">
+              <VideoCards
+                isvideoCardTypeHorizontal={true}
+                videoData={video}
+                isDropDownShow={true}
+              >
+                <LikedVideoRemoveButton
+                  videoID={video._id}
+                  buttonText={"Dislike"}
+                />
+              </VideoCards>
+            </div>
+          ))}
+      </section>
+    </div>
+  );
+}
